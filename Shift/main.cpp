02b892387cda6608ef93e13fa2d9c8c83c7eaa51
Home <Home@ERSHOVV>
2024-03-19 22:33:07 +0500

    ShiftLeft DONE

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -9,10 +9,10 @@ void main()
 	//cout << int() << endl;
 	//int() - значение по умолчанию для типа 'int'
 	//double() - значение по умолчанию для типа 'double'
-	
-	
+
+
 	const int n = 10;
-	int arr[n] = {0,1,1,2,3,5,8,13,21,34};
+	int arr[n] = { 0,1,1,2,3,5,8,13,21,34 };
 
 	// вывод исходного массива на экран:
 	for (int i = 0; i < n; i++)
@@ -20,18 +20,32 @@ void main()
 		cout << arr[i] << tab;
 	}
 	cout << endl;
-    
+
 
 	//сдвиг массива:
-	int k; 
-	cout << "количество сдвигов: ";cin >> k;
-	int shift = arr[0];
-	for (int i = 0; i < n; i++)
+	int number_of_shifts;
+	cout << " ввидете количество сдвигов:"; cin >> number_of_shifts;
+	for(int i = 0;i < number_of_shifts; i++)
+	{ 
+	int buffer = arr[0];
+	for (int i = 1; i < n; i++)
 	{
+		arr[i-1] = arr[i];
+	}
+	arr[n - 1] = buffer;
+    }
+	
+	/*int k = arr[n];
+	int shift=arr[0];
+
+	cout << "количество сдвигов: ";cin >> k;
+	
+	for (int i = 0; i < n; i++)
+	{   
 		arr[i - k] = arr[i];
 		arr[i] = shift;
-
 	}
+	cout << endl;*/
 	// вывод сдвинутного массива на эран
 	for (int i = 0; i < n; i++)
 	{
